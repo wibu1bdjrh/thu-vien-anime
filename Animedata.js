71958518d98f36c481d4661e58fce0398af7a271
thuvienanime <hungqwa12@gmail.com>
2025-04-12 09:34:12 +0700

    Update and rename Anime.js to Animedata.js

---
+++ Animedata.js
@@ -0,0 +1,31 @@
+// animeData.js
+const animeList = [
+  {
+    title: "Grand Blue",
+    genre: "Hài hước, Tình cảm, Học đường, Đời thường",
+    year: 2020,
+    image: "https://i.imgur.com/WdNeyg0.jpeg",
+    episodes: [
+      { id: "1TFM3VCKHLpuD8zBHiAVucn7LpMNARbNh", name: "Tập 1" },
+      { id: "1-7vXxMrG6dUVI-wJ19aDdWZeCPKaxZOM", name: "Tập 2" }
+    ]
+  },
+  {
+    title: "Your Name",
+    genre: "Lãng mạn",
+    year: 2016,
+    image: "https://i.imgur.com/oHgkI4h.jpg",
+    episodes: [
+      { id: "3KR8_igDs1Y", name: "Trailer" }
+    ]
+  },
+  {
+    title: "Attack on Titan",
+    genre: "Kịch tính",
+    year: 2013,
+    image: "https://i.imgur.com/tbn61U6.jpg",
+    episodes: [
+      { id: "MGRm4IzK1SQ", name: "Trailer" }
+    ]
+  }
+];
